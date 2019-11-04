1a398c11317093b2dbd405392fb91d0601b3ff25
DUY.TRAM <DUY.TRAM@s124-12.pedago.mermoz.edu>
2019-11-04 15:09:13 +0100

    add tp1 and tp2

--- src/tp2/Tp2Exo1.java
+++ src/tp2/Tp2Exo1.java
@@ -1,9 +1,21 @@
 package tp2;
 
+import java.util.Scanner;
+
 public class Tp2Exo1 {
 
 	public static void main(String[] args) {
-		System.out.print("TP2");
+		Scanner Saisir = new Scanner(System.in);
+		int valeur1;
+		int valeur2;
+		int valeur;
+		System.out.print("Entre nombre 1: ");
+		valeur1 = Saisir.nextInt();
+		System.out.println("Entre nombre 2: ");
+		valeur2 = Saisir.nextInt();
+		valeur = valeur1 + valeur2;
+		System.out.println("Total les 2 valeurs sont: "+ valeur);
+		Saisir.close();
 	}
 
 }
