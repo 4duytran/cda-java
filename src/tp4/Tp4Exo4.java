947dd0d55556da794af022bcb4a45f80d3e723b8
duytran <duyfr85@gmail.com>
2019-11-05 19:33:51 +0100

    Tp4 Exo4 Corrigé

--- src/tp4/Tp4Exo4.java
+++ src/tp4/Tp4Exo4.java
@@ -9,7 +9,7 @@ public class Tp4Exo4 {
 		String text = JOptionPane.showInputDialog(null, "Entrer un nombre: ");
 		int nb = Integer.parseInt(text);
 		int n = 0;
-		for (int i = 1; i < nb; i++) {
+		for (int i = 1; i <= nb; i++) {
 			int resultat = nb % i;
 			if (resultat == 0) {
 				n++;
