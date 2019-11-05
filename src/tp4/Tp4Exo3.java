346928125ff23e067609460a8be440abf5415c09
duytran <duyfr85@gmail.com>
2019-11-05 18:49:09 +0100

    Tp4 Exo3

---
+++ src/tp4/Tp4Exo3.java
@@ -0,0 +1,19 @@
+package tp4;
+
+import java.util.Scanner;
+
+public class Tp4Exo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Saisir = new Scanner(System.in);
+		System.out.println("Entrer un nombre: ");
+		int nb = Saisir.nextInt();
+		String chaine = "";
+		for (int i = 0; i < nb; i++) {
+			chaine += "*";
+			System.out.println(chaine);
+		}
+	}
+
+}
