021fc949b2619bfe293327d331cc295ce52a2346
DUY.TRAM <DUY.TRAM@s124-12.pedago.mermoz.edu>
2019-11-06 12:56:07 +0100

    Tp4 Exo6

--- src/tp4/Tp4Exo6.java
+++ src/tp4/Tp4Exo6.java
@@ -1,10 +1,38 @@
 package tp4;
 
+import javax.swing.JOptionPane;
+
 public class Tp4Exo6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String text = JOptionPane.showInputDialog(null, "Entrer un nombre: ");
+		int saisir = Integer.parseInt(text);
+		int nb = 0;
+		int n = 1;
+		int somme = 0;
+		String chaine = "";
+		while (nb < saisir) {
+			n++;
+			if (nombrePremier(n)) {
+				nb++;
+				chaine += n;
+				chaine += " + ";
+				somme += n;
+			}			
+		}
+		JOptionPane.showMessageDialog(null, chaine.substring(0, chaine.length() - 2) + " = " + somme);
 
+	}
+	
+	public static boolean nombrePremier(int nb) {
+		boolean flag = true;
+		for (int i = 2; i < nb; i++) {
+			if (nb%i==0) {
+				flag = false;
+			}
+		}
+		return flag;
 	}
 
 }
