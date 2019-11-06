3a0b01a7c4fd616ec543fc91379c35d71f8f34c7
duytran <duyfr85@gmail.com>
2019-11-06 21:53:24 +0100

    Tp4 Exo7 fini

--- src/tp4/Tp4Exo7.java
+++ src/tp4/Tp4Exo7.java
@@ -7,10 +7,19 @@ public class Tp4Exo7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String text = JOptionPane.showInputDialog(null, "Saisir un nombre: ");
-		int nb = Integer.parseInt(text);
-		int bit = convertBinary(nb);
-		JOptionPane.showMessageDialog(null, bit);
+		String text = JOptionPane.showInputDialog(null, "Saisir un nombre Decimal: ");
+		int n = Integer.parseInt(text);
+		int binaire = convertBinary(n);
+		String text_2 = JOptionPane.showInputDialog(null, "Saisir un nombre de Bit: ");
+		int bit = Integer.parseInt(text_2);
+		int mask = 1 << bit;
+		if ((n & mask) == 0) {
+			JOptionPane.showMessageDialog(null,"Le decimal " + n + " est égale à " + binaire + " en Binaire avec le Bit " + bit + " est égale à 0");
+		}
+		else if ((n & mask) != 0)  {
+			JOptionPane.showMessageDialog(null, "Le decimal " + n + " est égale à " + binaire + " en Binaire avec le Bit " + bit + " est égale à 1");
+		}
+		
 	}
 	
 	
