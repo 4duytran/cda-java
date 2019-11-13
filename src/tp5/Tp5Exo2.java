97819bf2de88bce482beeafb7e86a87c4554e514
DUY.TRAM <DUY.TRAM@s124-12.pedago.mermoz.edu>
2019-11-13 17:58:42 +0100

    Tp5 Exo2

---
+++ src/tp5/Tp5Exo2.java
@@ -0,0 +1,28 @@
+package tp5;
+
+public class Tp5Exo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[][]={{1,2,3},{1,2,3},{1,2,3}};    
+		int b[][]={{4,5,6},{4,5,6},{4,5,6}}; 
+		int c[][]=new int[3][3];   
+	      
+		for(int i=0;i<3;i++)
+		{    
+			for(int j=0;j<3;j++)
+			{    
+				c[i][j]=0;      
+				for(int k=0;k<3;k++)      
+				{      
+				c[i][j]+=a[i][k]*b[k][j];      
+				} 
+				System.out.print(c[i][j]+" ");  
+			} 
+			System.out.println();  
+		} 
+		
+			
+	}
+
+}
