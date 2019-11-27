cff39f696d84740f5615b218cb01d6195750a37a
Duy <56829213+4duytran@users.noreply.github.com>
2019-11-27 19:25:02 +0100

    Update Tp6Exo3.java

--- src/tp6/Tp6Exo3.java
+++ src/tp6/Tp6Exo3.java
@@ -8,16 +8,19 @@ public class Tp6Exo3 {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Entrer une chaine de cacactère: ");
+		System.out.println("Entrer une chaine de cacactÃ¨re: ");
 		String maChaine = sc.nextLine();
 		String autreChaine ="";
+		StringBuilder buffer = new StringBuilder();
 		
 		for (int i=(maChaine.length()-1); i>=0; i--)
 		{
-			autreChaine += maChaine.charAt(i);
+			buffer.append(maChaine.charAt(i));
 		}
+		autreChaine = buffer.toString();
 		System.out.print(autreChaine);
 		sc.close();
 	}
 
 }
+
